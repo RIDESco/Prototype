8bc3bd13f8ccf88097431302e2303120d5d7671c
Chris Elles <chriselles@alumni.gsb.stanford.edu>
2020-09-21 17:17:43 +1200

    First commit: Big Red/Green Button & Nav

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
+var x = document.getElementById("demo");
+ 
+  function getLocation() {
+   if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(showPosition);
+   } else {
+     x.innerHTML = "Geolocation is not supported by this browser.";
+   }
+  }
+   
+  function showPosition(position) {
+   x.innerHTML = "Latitude: " + position.coords.latitude +
+   "<br>Longitude: " + position.coords.longitude;
+  }
